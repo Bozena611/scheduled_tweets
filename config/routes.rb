1057311abd83c29e7bfdc0bc5753f29a027d5104
Bozena11 <bozena.vuckovic@gmail.com>
2021-12-07 14:59:47 +0100

    fix webpacker, add about and main vc

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # get "/", to: "main#index" BUT BELOW MORE COMMON
+
+  root to: "main#index"
+
+  # GET /about
+  get "/about", to: "about#index"
+
 end

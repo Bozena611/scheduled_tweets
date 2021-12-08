ad35ae0ca0d2e3c551441def5f667314a7e9845e
Bozena11 <bozena.vuckovic@gmail.com>
2021-12-08 12:51:44 +0100

    add login and logout functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+	before_action :set_current_user
+	
+	def set_current_user
+		if session[:user_id]
+			# User.find(session[:user_id]) throws error if user does not exist
+			# so we use code below with find_by
+			Current.user = User.find_by(id: session[:user_id])
+		end
+	end
 end
